f64f9fca22fe399a186dc57bc6ffb8cff81e96f3
Jiwon <js6450@nyu.edu>
2018-09-30 21:28:55 -0400

    added more randInt, arrayLen, factorize and 3D distance calc funtions and its tests

--- sketch.js
+++ sketch.js
@@ -29,6 +29,43 @@ function anomalyCode(x) {
   return '5' + x - x;
 }
 
+var numArray = [];
+var arrayLen = 10;
+
+function setArray(arrayLen){
+  for(var i = 0; i < arrayLen; i++){
+    numArray[i] = randInt(0, 100);
+  }
+
+  return numArray;
+}
+
+function randInt(min, max){
+    return Math.round(Math.random(min, max));
+}
+
+function factorize(num){
+  var totalProduct = 1;
+
+  for(var i = 1; i < num + 1; i++){
+    totalProduct *= i;
+  }
+
+  return totalProduct
+}
+
+class Point3D{
+  constructor(x, y, z){
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+}
+
+function inDist(p1, p2, d){
+  return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y) + (p1.z - p2.z) * (p1.z - p2.z) < d * d;
+}
+
 module.exports = {
   sum: sum,
   sub: sub,
@@ -37,5 +74,10 @@ module.exports = {
   digital_root: digital_root,
   sayHelloTo: sayHelloTo,
   answer: answer,
-  anomalyCode: anomalyCode
+  anomalyCode: anomalyCode,
+  setArray: setArray,
+  randInt: randInt,
+  factorize: factorize,
+  Point3D: Point3D,
+  inDist: inDist
 }
